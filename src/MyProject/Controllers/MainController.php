6c399e553ec2e539b8959a228ed97ebd01d972f1
poster834 <poster834@gmail.com>
2022-07-15 15:11:30 +0300

    new commit 15-07-22

---
+++ src/MyProject/Controllers/MainController.php
@@ -0,0 +1,10 @@
+<?php
+namespace MyProject\Controllers;
+
+class MainController
+{
+    public function main()
+    {
+        echo "Главная страница";
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function myAutoLoader(string $className) 
+{
+    require_once __DIR__.'/../src/'. str_replace('/', '\\',$className).'.php';
+}
+spl_autoload_register('myAutoLoader');
+
+
+
+$controller = new \MyProject\Controllers\MainController();
+$controller->main();
