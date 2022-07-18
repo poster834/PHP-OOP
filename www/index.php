c1b6bf99ec1080aa00c72129e58fc89ae78afd0d
poster834 <poster834@gmail.com>
2022-07-18 23:37:22 +0300

    masterHome

--- www/index.php
+++ www/index.php
@@ -6,33 +6,5 @@ function myAutoLoader(string $className)
 }
 spl_autoload_register('myAutoLoader');
 
-$route = $_GET['route'] ?? '';
-
-
-$routes = require __DIR__ .  str_replace('/', '\\','/../src/routes.php');
-
-
-$isRouteFound = false;
-
-foreach ($routes as $pattern => $controllerAndAction) {
-    preg_match($pattern, $route, $matches);
-    if (!empty($matches)) {
-        $isRouteFound = true;
-        break;
-    }
-}
-
-if (!$isRouteFound) {
-    echo 'Страница не найдена';
-    return;
-}
-unset($matches[0]);
-// echo "<pre>";
-// var_dump($controllerAndAction);
-// var_dump($matches);
-
-$controllerName = $controllerAndAction[0];
-$actionName = $controllerAndAction[1];
-$controller = new $controllerName();
-$controller -> $actionName(...$matches);
-
+$controller = new \MyProject\Controllers\MainController();
+$controller->main();
