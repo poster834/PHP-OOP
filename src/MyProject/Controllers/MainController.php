fb393cc2cf14e6a6b864eebf93b59fa75e7a2a5d
poster834 <poster834@gmail.com>
2022-07-20 23:39:37 +0300

    20-07-22Home

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -1,24 +1,31 @@
 <?php
 
 namespace MyProject\Controllers;
+
+use MyProject\Services\Db;
 use MyProject\View\View;
 
 class MainController
 {
     private $view;
+    private $db;
+
     public function __construct()
     {
         $this->view = new View(__DIR__. str_replace('/','\\','/../../templates/'));
+        $this->db = new Db();
     }
 
 
     public function main()
     {
-        $articles = [
-            ['name' => 'Статья1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья2', 'text' => 'Текст статьи 2'],
-            ['name' => 'Статья3', 'text' => 'Текст статьи 3'],
-        ];
+        $articles = $this->db->query('SELECT * FROM `articles`;');
+
+        // $articles = [
+        //     ['name' => 'Статья1', 'text' => 'Текст статьи 1'],
+        //     ['name' => 'Статья2', 'text' => 'Текст статьи 2'],
+        //     ['name' => 'Статья3', 'text' => 'Текст статьи 3'],
+        // ];
         $this->view->renderHtml('main/main.php',['articles'=>$articles]);
 
     }
---
+++ src/MyProject/Services/Db.php
@@ -0,0 +1,30 @@
+<?php
+namespace MyProject\Services;
+
+use PDO;
+
+class Db
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $dbOptions = (require __DIR__.'/../../settings.php')['db'];
+        $this->pdo = new PDO(
+            'mysql:host='.$dbOptions['host'].
+            '; dbname='.$dbOptions['dbname'], $dbOptions['user'],$dbOptions['password']
+        );
+        $this->pdo->exec('SET NAMES UTF8');
+    }
+
+    public function query(string $sql, $params = []): ?array
+    {
+        $sth = $this->pdo->prepare($sql);
+        $result = $sth->execute($params);
+
+        if ($result === false) {
+            return null;
+        }
+        return $sth->fetchAll();
+    }
+}
