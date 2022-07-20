4261672404d0f8816cb820fc28746c62b9a3dd9f
poster834 <poster834@gmail.com>
2022-07-20 15:48:53 +0300

    20-07-22_17:30

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -1,17 +1,30 @@
 <?php
 
 namespace MyProject\Controllers;
+use MyProject\View\View;
 
 class MainController
 {
+    private $view;
+    public function __construct()
+    {
+        $this->view = new View(__DIR__. str_replace('/','\\','/../../templates/'));
+    }
+
+
     public function main()
     {
-        include __DIR__. str_replace('/','\\','/../../templates/main/main.php');
-        
+        $articles = [
+            ['name' => 'Статья1', 'text' => 'Текст статьи 1'],
+            ['name' => 'Статья2', 'text' => 'Текст статьи 2'],
+            ['name' => 'Статья3', 'text' => 'Текст статьи 3'],
+        ];
+        $this->view->renderHtml('main/main.php',['articles'=>$articles]);
+
     }
 
     public function sayHello(string $name)
     {
-        echo 'Привет, '.$name; 
+        $this->view->renderHtml('main/hello.php',['name'=>$name]);
     }
 }
--- src/templates/main/main.php
+++ src/templates/main/main.php
@@ -1,25 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../www/style.css">
-    <title>Блог</title>
-</head>
-<body>
+<?php include('../src/templates/header.php');?>
+
     <table class='layout'>
         <tr>
             <td colspan='2' class='header'>
                 Мой блог
             </td>
         </tr>
+        <?php foreach ($articles as $article):?>
         <tr>
             <td>
-                <h2></h2>
-                <p></p>
+                <h2><?=$article['name'];?></h2>
+                <p><?=$article['text'];?></p>
             </td>
         </tr>
-    </table>
-</body>
-</html>
+        <?php endforeach;?>
+        
+        <?php include('../src/templates/footer.php');?>
